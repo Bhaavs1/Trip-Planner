fb2416c3c224f6df0ba06e7f1b2be9bca3a0ad66
Bhaavya Arora <bhaavyaarora@Bhaavyas-MacBook-Pro.local>
2024-10-14 08:21:02 +1030

    ?

---
+++ Item.h
@@ -0,0 +1,23 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Item
+{
+private:
+    /* data */
+public:
+    Item();
+    Item(string name);
+    Item(string name, );
+    ~Item();
+};
+
+
+
+
+#endif
