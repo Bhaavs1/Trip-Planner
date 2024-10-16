01fcf81991b6918f1d5e8a74c51924f21b661290
Bhaavya Arora <bhaavyaarora@Bhaavyas-MacBook-Pro.local>
2024-10-16 16:41:02 +1030

    Updated test-1-3.cpp

---
+++ test-1-3.cpp
@@ -0,0 +1,28 @@
+#include "Destination.h"
+#include <vector>
+
+int main() {
+    Destination d1("Helsinki", "Finland");
+    Destination d2("Kinshasa", "DRC");
+    Destination d3("Montevideo", "Uruguay");
+
+    vector<Destination> destinations;
+    destinations.push_back(d1);
+    destinations.push_back(d2);
+    destinations.push_back(d3);
+
+    for (int i = 0; i < destinations.size(); i++)
+    {
+        cout << destinations[i].get_city() << endl;
+    }
+    
+    destinations[0].set_arrival_date("20/10/24");
+    destinations[0].set_departure_date("5/12/24");
+    destinations[0].set_residence("123 Happy Way, Pieburg");
+    destinations[0].set_subdivision("Uusimaa");
+
+    cout << destinations[0].get_arrival_date() << endl;
+    cout << destinations[0].get_departure_date() << endl;
+    cout << destinations[0].get_residence() << endl;
+    cout << destinations[0].get_subdivision() << endl;
+}
